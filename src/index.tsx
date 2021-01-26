46cae3cee84dec0223f2b99c171598fa1872aca1
danicolms.c <=>
2021-01-26 18:11:49 +0100

    Yarn library working

--- src/index.tsx
+++ src/index.tsx
@@ -16,10 +16,25 @@ const TestComponent = () => {
   return <div style={{ height: "100vh" }}> Im a test component</div>;
 };
 
+const SUCCESS = (message) => (
+  <div style={{ background: "green" }}> {message}</div>
+);
+const ERROR = (message) => <div style={{ background: "red" }}> {message}</div>;
+const WARNING = (message) => (
+  <div style={{ background: "yellow" }}> {message}</div>
+);
+const INFO = (message) => <div style={{ background: "blue" }}> {message}</div>;
 class App extends React.Component {
   render() {
     return (
-      <ToastProvider>
+      <ToastProvider
+        toastComponents={{
+          success: SUCCESS,
+          error: ERROR,
+
+         
+        }}
+      >
         <TestComponent></TestComponent>
       </ToastProvider>
     );
